1ca23c4b176d4e0ce975d77aa79740a36d0b97c6
Tomas Santucho <tomassantucho98@gmail.com>
2022-11-08 16:38:11 -0300

    Add spring and maven

--- src/main/java/com/digitalhouse/ej1/model/Empleado.java
+++
@@ -1,6 +0,0 @@
-package com.digitalhouse.ej1.model;
-
-import java.io.Serializable;
-
-public record Empleado(String nombre, String apellido, String legajo, int sueldo) implements Serializable {
-}
--- src/main/java/com/digitalhouse/ej1/model/Empresa.java
+++
@@ -1,7 +0,0 @@
-package com.digitalhouse.ej1.model;
-
-import java.io.Serializable;
-import java.util.List;
-
-public record Empresa(String cuit, String razonSocial, List<Empleado> empleados) implements Serializable {
-}
