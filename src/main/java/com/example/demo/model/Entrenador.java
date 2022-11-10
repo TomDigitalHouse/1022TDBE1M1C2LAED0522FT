2844268de93f7cc1a52c8324eb359dcddf1c2896
Tomas Santucho <tomassantucho98@gmail.com>
2022-11-10 15:15:37 -0300

    Project setup

---
+++ src/main/java/com/example/demo/model/Entrenador.java
@@ -0,0 +1,4 @@
+package com.example.demo.model;
+
+public record Entrenador(String nombre) {
+}
---
+++ src/main/java/com/example/demo/service/EntrenadorService.java
@@ -0,0 +1,9 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Entrenador;
+
+import java.util.List;
+
+public interface EntrenadorService {
+    List<Entrenador> getEntrenadores();
+}
