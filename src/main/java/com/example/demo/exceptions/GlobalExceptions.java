8554c78a4d930996b7f2cdf91a1b6c9958d4b9ed
Tomas Santucho <tomassantucho98@gmail.com>
2022-12-04 19:19:38 -0300

    Fixes

--- src/main/java/com/example/demo/exceptions/GlobalExceptions.java
+++ src/main/java/com/example/demo/exceptions/GlobalExceptions.java
@@ -1,5 +1,7 @@
 package com.example.demo.exceptions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,14 +10,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptions {
+    private final static Logger logger = LoggerFactory.getLogger(GlobalExceptions.class);
 
     @ExceptionHandler({ResourceNotFoundException.class})
     public ResponseEntity<String> error(ResourceNotFoundException exception){
+        logger.error(exception.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
     }
 
     @ExceptionHandler({PacienteNotFoundException.class})
     public ResponseEntity<String> error(PacienteNotFoundException exception){
+        logger.error(exception.getMessage());
         return ResponseEntity.status(HttpStatus.BANDWIDTH_LIMIT_EXCEEDED).body(exception.getMessage());
     }
 
